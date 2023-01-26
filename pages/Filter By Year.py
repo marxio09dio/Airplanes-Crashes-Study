7f9ddd182c21ee9cf654bb8ce4f0400232d8f063
marxio09dio <38763806+marxio09dio@users.noreply.github.com>
2023-01-26 18:33:10 +0000

    changes to page

--- pages/Filter By Year.py
+++ pages/Filter By Year.py
@@ -2,8 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title='Airplanes Crash Data', layout="wide")
-
+st.title("Filter By Year")
 
 df = pd.read_excel(io='plane_crash_info_cleaned.xlsx', sheet_name='Sheet1',usecols='A:Q', nrows=5064)
 
@@ -25,33 +24,6 @@ df_selection = df.query("year >= @year[0] and year <= @year[1]")
 
 
 #------Page total KPIs------
-st.title(":red[All Accidents] from 1908 to 2023")
-st.markdown('##')
-
-total_accidentes = len(df)
-total_aboard_fatalities = int(df['Aboard_Fatalities'].sum())
-total_ground_fatalities = int(df['Ground_Fatalities'].sum())
-total_fatalities = int(df['Total_Fatalites'].sum())
-
-left_columnn, middle_column, md_column, right_column = st.columns(4)
-with left_columnn:
-    st.metric(label='Number of Accidents', value=total_accidentes)
-    #st.subheader('Total Accidents')
-    #st.subheader(f"{total_accidentes:,}")
-with middle_column:
-    st.metric(label='Aboard Fatalities', value=total_aboard_fatalities)
-    #st.subheader('Aboard Fatalities')
-    #st.subheader(total_aboard_fatalities)
-with md_column:
-    st.metric(label='Ground Fatalities', value=total_ground_fatalities)
-    #st.subheader('Ground Fatalities')
-    #st.subheader(total_ground_fatalities)
-with right_column:
-    st.metric(label='Total Fatalites', value=total_fatalities)
-    #st.subheader('Total Fatalites')
-    #st.subheader(total_fatalities)
-
-
 st.markdown('---')
 
 
